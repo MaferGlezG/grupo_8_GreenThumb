c8960317dce23809aca1c969aaee880cd11d4b9a
MaFerGlezG <16012603@udec.edu.mx>
2021-06-15 21:42:38 -0500

    agregando el resto de las páginas

--- app.js
+++ app.js
@@ -8,5 +8,17 @@ app.listen(3030, ()=>{
 });
 
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/product-Cart', (req,res)=>{
+    res.sendFile(__dirname + '/views/productCart.html');
+});
+
+app.get('/product-Detail', (req,res)=>{
+    res.sendFile(__dirname + '/views/productDetail.html');
+});
+
+app.get('/product-Showcase', (req,res)=>{
+    res.sendFile(__dirname + '/views/productShowcase.html');
 });
