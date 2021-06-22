b9a4c119382369bacdc5d5f2b20e753098751200
r0cio <rocio.juarez10@hotmail.com>
2021-06-22 02:49:38 -0500

    Avance de register.css

--- app.js
+++ app.js
@@ -23,10 +23,6 @@ app.get('/product-Showcase', (req,res)=>{
     res.sendFile(__dirname + '/views/productShowcase.html');
 });
 
-<<<<<<< HEAD
 app.get('/register', (req,res)=>{
-=======
-app.get('/register-login', (req,res)=>{
->>>>>>> 3d75f2f5bcaec73030ecc0a34316d20833392dbf
     res.sendFile(__dirname + '/views/register.html');
 });
