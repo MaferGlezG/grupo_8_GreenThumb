234975915803876a3f5e06511929274efc6087c9
MaFerGlezG <16012603@udec.edu.mx>
2021-07-15 20:12:00 -0500

    agregando rutas y modificando controlador de productos

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,18 +1,37 @@
-let productController ={
-    detail : (req, res) => {
+let productController = {
+    detail: (req, res) => {
         res.render('productDetail');
     },
 
-    cart : (req, res) => {
+    cart: (req, res) => {
         res.render('productCart');
     },
-    showcase : (req, res) => {
+    showcase: (req, res) => {
         res.render('productShowcase');
     },
 
-    add : (req, res) => {
+    add: (req, res) => {
+        res.render('productAdd');
+    },
+
+    /*
+    
+    store: (req, res) => {
+        res.render('productDetail');
+    },
+
+    edit: (req, res) => {
+        res.render('productAdd');
+    },
+
+    update: (req, res) => {
+        res.render('productAdd');
+    },
+    destroy: (req, res) => {
         res.render('productAdd');
     }
+    
+    */
 }
 
 module.exports = productController;
--- src/routers/productRouter.js
+++ src/routers/productRouter.js
@@ -4,9 +4,30 @@ const router = express.Router();
 
 const productController = require('../controllers/productController');
 
-router.get('/view/:id', productController.detail);
-router.get('/cart', productController.cart);
+//Listado de productos (product showcase)
 router.get('/', productController.showcase);
-router.get('/add', productController.add);
+
+//Era add, formulario de creación de productos
+router.get('/create', productController.add);
+
+//Detalle de un producto
+router.get('/:id/view', productController.detail);
+
+//Carrito
+router.get('/cart', productController.cart);
+
+/*
+
+//Acción de creación (a donde se envía el formulario)
+router.post('/', productController.store); 
+
+//editar un producto
+router.get('/:id/edit', productController.edit); 
+router.put('/:id/view', productController.update); 
+
+//Eliminar un producto
+router.delete('/:id', productController.destroy);
+
+*/
 
 module.exports = router;
