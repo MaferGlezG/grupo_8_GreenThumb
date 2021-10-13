3fd33526cca1349dcb4006455540818be4740e0c
MaFerGlezG <16012603@udec.edu.mx>
2021-10-13 16:14:20 -0500

    Implementando base de datos en index, ajustando estilos en productShowcase

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,6 +1,13 @@
-let mainController ={
-    index : (req, res) => {
-        res.render('index');
+const db = require('../database/models');
+const Producto = require('../database/models/Producto');
+
+let mainController = {
+    index: (req, res) => {
+        db.Producto.findAll()
+            .then(function (products) {
+                res.render('index', { products: products });
+            })
+
     },
 
 }
